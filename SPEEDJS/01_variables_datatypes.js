f949c5ef831d4d7212a6878f9e2f6640a3f99977
santwan <santwanpathak1@gmail.com>
2025-03-07 21:17:55 +0530

    Update 01_variables_datatypes.js

--- SPEEDJS/01_variables_datatypes.js
+++ SPEEDJS/01_variables_datatypes.js
@@ -16,45 +16,60 @@ let symbolVar = Symbol() //symbol
 // console.log(typeof undefinedVar)
 // console.log(typeof nothing)
 
-let person = {
-    name:'Hitesh',
-    age:19,
-    isStudent:true,
+// let person = {
+//     name:'Hitesh',
+//     age:19,
+//     isStudent:true,
     
-}
+// }
 
-let num = "42"
-let convertedNum = Number(num)
+// let num = "42"
+// let convertedNum = Number(num)
 
-console.log(convertedNum) //output 42
-console.log(typeof convertedNum) //type is number
+// console.log(convertedNum) //output 42
+// console.log(typeof convertedNum) //type is number
 
+//------------------------------------------------------------------
+// let num = "42a"
+// let convertedNum = Number(num) // most standard way to convert
+
+// console.log(convertedNum) //output NaN
+// console.log(typeof convertedNum) //type is number
+
+// ---------------------------------------------------------------
 let num = "42a"
-let convertedNum = Number(num)
+// let convertedNum = Number(num) ( another way to do it)
+// let convertedNum = +num
+let convertedNum = parseInt(num)
+//above this different way to convert into number
 
-console.log(convertedNum) //output 42
+console.log(convertedNum) //output NaN
 console.log(typeof convertedNum) //type is number
 
+//----------------------------------------------------------------------
+let str = 123
+let convertedString = String(str)
 
 
 
-let hello()
 
-let sum = a + b
-let difference = a - b 
-let product = a * b 
-let quotient = a / b
-let remainder = a % b
-let power = a ** b
+// let hello()
 
-let x = 6
-let y = 10
+// let sum = a + b
+// let difference = a - b 
+// let product = a * b 
+// let quotient = a / b
+// let remainder = a % b
+// let power = a ** b
 
-console.log( x == y )
-console.log( x === y ) // checking datatypes
-console.log( x != y )
-console.log( x < y )
-console.log( x > y )
-console.log( x <= y )
-console.log( x >= y )
+// let x = 6
+// let y = 10
+
+// console.log( x == y )
+// console.log( x === y ) // checking datatypes
+// console.log( x != y )
+// console.log( x < y )
+// console.log( x > y )
+// console.log( x <= y )
+// console.log( x >= y )
 
