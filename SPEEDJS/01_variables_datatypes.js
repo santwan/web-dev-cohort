c06d6a8a3746cb74b6699705725c82b231b29077
santwan <santwanpathak1@gmail.com>
2025-03-07 21:05:53 +0530

    Update 01_variables_datatypes.js

--- SPEEDJS/01_variables_datatypes.js
+++ SPEEDJS/01_variables_datatypes.js
@@ -1,6 +1,45 @@
 let a = 10
 let b = 3
 
+let name = "hitesh"
+const pi = 3.14
+
+
+//Primitive datatype
+let number = 32 // Number
+let text = "Hello" //String
+let isTrue = true //Boolean
+let nothing = null // Object( null datatype technically)
+let undefinedVar = undefined // undefined
+let symbolVar = Symbol() //symbol
+
+// console.log(typeof undefinedVar)
+// console.log(typeof nothing)
+
+let person = {
+    name:'Hitesh',
+    age:19,
+    isStudent:true,
+    
+}
+
+let num = "42"
+let convertedNum = Number(num)
+
+console.log(convertedNum) //output 42
+console.log(typeof convertedNum) //type is number
+
+let num = "42a"
+let convertedNum = Number(num)
+
+console.log(convertedNum) //output 42
+console.log(typeof convertedNum) //type is number
+
+
+
+
+let hello()
+
 let sum = a + b
 let difference = a - b 
 let product = a * b 
