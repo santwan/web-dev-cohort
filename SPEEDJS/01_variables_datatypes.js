b2e7c11764610de0ac454be4f22626aea9dbf3f3
santwan <santwanpathak1@gmail.com>
2025-03-07 23:40:15 +0530

    Update 01_variables_datatypes.js

--- SPEEDJS/01_variables_datatypes.js
+++ SPEEDJS/01_variables_datatypes.js
@@ -1,20 +1,22 @@
-let a = 10
-let b = 3
+// let a = 10
+// let b = 3
 
-let name = "hitesh"
-const pi = 3.14
+// let name = "hitesh"
+// const pi = 3.14
 
 
-//Primitive datatype
-let number = 32 // Number
-let text = "Hello" //String
-let isTrue = true //Boolean
-let nothing = null // Object( null datatype technically)
-let undefinedVar = undefined // undefined
-let symbolVar = Symbol() //symbol
+// //Primitive datatype
+// let number = 32 // Number
+// let text = "Hello" //String
+// let isTrue = true //Boolean
+// let nothing = null // Object( null datatype technically)
+// let undefinedVar = undefined // undefined
+// let symbolVar = Symbol() //symbol
 
 // console.log(typeof undefinedVar)
 // console.log(typeof nothing)
+
+//---------------------------------------------------------------------
 
 // let person = {
 //     name:'Hitesh',
@@ -23,6 +25,7 @@ let symbolVar = Symbol() //symbol
     
 // }
 
+//---------------------------------------------------------------------
 // let num = "42"
 // let convertedNum = Number(num)
 
@@ -37,30 +40,28 @@ let symbolVar = Symbol() //symbol
 // console.log(typeof convertedNum) //type is number
 
 // ---------------------------------------------------------------
-let num = "42a"
-// let convertedNum = Number(num) ( another way to do it)
-// let convertedNum = +num
-let convertedNum = parseInt(num)
-//above this different way to convert into number
+// let num = "42a"
+// // let convertedNum = Number(num) ( another way to do it)
+// // let convertedNum = +num 
+// let convertedNum = parseInt(num)
+// //above this different way to convert into number
 
-console.log(convertedNum) //output NaN
-console.log(typeof convertedNum) //type is number
+// console.log(convertedNum) //output NaN
+// console.log(typeof convertedNum) //type is number
 
 //----------------------------------------------------------------------
-let str = 123
-let convertedString = String(str)
+// let str = 123
+// let convertedString = String(str)
 
-
-
-
-// let hello()
-
-// let sum = a + b
-// let difference = a - b 
-// let product = a * b 
-// let quotient = a / b
-// let remainder = a % b
-// let power = a ** b
+//----------------------------------------------------------------------
+/*
+let sum = a + b
+let difference = a - b 
+let product = a * b 
+let quotient = a / b
+let remainder = a % b
+let power = a ** b
+*/
 
 // let x = 6
 // let y = 10
@@ -73,3 +74,14 @@ let convertedString = String(str)
 // console.log( x <= y )
 // console.log( x >= y )
 
+//---------------------------------------------------------------------------------
+console.log( Math.random() * 10)
+
+let firstName = "Samtwam"
+let lastName = "Pathak"
+
+let fullName = firstName + " " + lastName
+
+let message = "Hello World"
+console.log(message.length)
+console.log(message.toLowerCase())
