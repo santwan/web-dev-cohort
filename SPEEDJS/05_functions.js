61f30da25348d7058176a7f9f4fe32683fa8efd2
santwan <santwanpathak1@gmail.com>
2025-03-14 10:56:17 +0530

    Update 05_functions.js

--- SPEEDJS/05_functions.js
+++ SPEEDJS/05_functions.js
@@ -45,4 +45,29 @@ let person2 = {
 
 // person1.greet.call(person2)  
 
+//----------------------------------------------------------------------------------------------
+const obj = {
+    personName: "Santwan", 
+    greet: function(){
+        console.log(`Hello, ${this.personName}`)
+    },
+}
+
+
+console.log("Hello Bye bye")
+
+setTimeout(obj.greet, 1000 * 3)
+
+setTimeout(function(){
+    console.log("Hello")
+}, 1000*5 /*Second argument in ms */)
+
+
+a = 10 , b = 12
+console.log('sum', a + b )
+
+//Blog on call stack concept in Javascript
+
+// call stack waits for  nothing 
+
 
