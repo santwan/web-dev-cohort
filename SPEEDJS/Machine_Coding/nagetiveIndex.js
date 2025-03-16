1e9a40418906ad4f0a14bfb3401b0a985806114c
santwan <santwanpathak1@gmail.com>
2025-03-16 11:14:29 +0530

    Updated

--- SPEEDJS/Machine_Coding/nagetiveIndex.js
+++ SPEEDJS/Machine_Coding/nagetiveIndex.js
@@ -2,13 +2,26 @@ let arr = [1,2,3,4,45,6,7,8,9,0]
 
 console.log(arr[-1])
 
-const user = {
-    name: ' Hitesh',
-    age: 55,
-    password: "ABc@123"
-}
+// const user = {
+//     name: ' Hitesh',
+//     age: 55,
+//     password: "ABc@123",
+// }
 
 // const proxyUser = user
 // proxyUser.password
 //the goal is to hide the user password 
-const proxyUser = new Proxy(user, handler)
+// const proxyUser = new Proxy(user, {
+//     get(target, prop){ //prop = property , target is the target object
+//         if(prop === 'password'){
+//             throw new Error("Access Denied")
+//         }
+
+//         return target[prop]
+//     },
+//     set(target, prop, user){
+
+//     }
+// })
+
+// console.log(proxyUser.password)
---
+++ SPEEDJS/Machine_Coding/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+password: "ABc@123",
