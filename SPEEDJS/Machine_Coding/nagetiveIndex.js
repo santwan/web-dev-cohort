1128237ac7669d5f0ac6509e22e86adf43894f14
santwan <santwanpathak1@gmail.com>
2025-03-16 11:45:43 +0530

    Update nagetiveIndex.js

--- SPEEDJS/Machine_Coding/nagetiveIndex.js
+++ SPEEDJS/Machine_Coding/nagetiveIndex.js
@@ -1,6 +1,6 @@
 let arr = [1,2,3,4,45,6,7,8,9,0]
 
-console.log(arr[-1])
+
 
 // const user = {
 //     name: ' Hitesh',
@@ -25,3 +25,32 @@ console.log(arr[-1])
 // })
 
 // console.log(proxyUser.password)
+
+function nagetiveIndex(arr) {
+    return new Proxy(arr, {
+        get(target, prop){
+            const index = Number(prop)
+            if( index < 0 ){
+                return target[target.length + index]
+            }
+            return target[index]
+        },
+        set(target, prop, value){
+            const index = Number(prop)
+            if(index<0){
+                target[index.length + index] = value
+            } else {
+                target[index] = value
+            }
+            return true
+        }
+    })
+}
+
+let newArr = nagetiveIndex(arr)
+
+console.log(newArr[-1])
+
+newArr[-1] = 22
+console.log(newArr)
+console.log(arr) // Shallow copy and deep copy concept
