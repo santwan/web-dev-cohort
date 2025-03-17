0a260063ea6a5a501cb177dbfce45c8495e86cd2
santwan <santwanpathak1@gmail.com>
2025-03-18 00:17:48 +0530

    updated

--- FULLSTACK/index.js
+++ FULLSTACK/index.js
@@ -1,5 +1,17 @@
 // require  - 
 // import  - If you are using import then you have to go to package.json file and have to make some changes - Add "type": "module"  in the object.
 
+// const express = require('express')
+import express from "express"
 
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
