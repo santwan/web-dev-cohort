60ea85b54b80ff8bb7bc526bb69ad4ca48e00ff5
santwan <santwanpathak1@gmail.com>
2025-03-18 19:47:41 +0530

    Update user.model.js

--- FULLSTACK/model/user.model.js
+++ FULLSTACK/model/user.model.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose"
 
-const userScenma = new mongoose.Schema()
+const userScenma = new mongoose.Schema({
+    name: String,
+    email: String,
+})
 
 
 const User  = mongoose.model("User", userScenma)
