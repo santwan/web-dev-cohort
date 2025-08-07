efde30f2840c8c1ac25c65a122991c9c632ff712
santwan <santwanpathak1@gmail.com>
2025-08-08 01:31:02 +0530

    Add auth controller with user registration logic
    Introduces an authentication controller with endpoints for user registration, login, verification, logout, forgot password, and reset password. Implements user registration with validation, password hashing, and email verification token generation. Other endpoints are stubbed with console logs.

---
+++ Phase 3 Node Ninja/Fullstack-sql/controllers/auth.controller.js
@@ -0,0 +1,80 @@
+import prisma from '../prisma/prismaClient.js';
+import bcrypt from 'bcrypt'
+import crypto from 'crypto';
+
+const registerUser = async (req , res ) => {
+    try {
+
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({ 
+                success: false,
+                message: "Please provide all required fields: name, email, and password" 
+            });
+        }
+
+        
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        });
+
+        if (!existingUser) {
+            return res.status(400).json({ 
+                success: false,
+                message: "User already exists" 
+            });
+        }
+
+        const hashedPassword = bcrypt.hash(password, 10)
+        const verificationToken = crypto.randomBytes(32).toString('hex')
+        
+        
+        const newUser = await prisma.user.create({
+            data: {
+                name: name,
+                email: email,
+                password : hashedPassword,
+                emailVerificationToken: verificationToken
+            }
+        });
+
+        return res.status(201).json({ message: "User registered successfully", user: newUser });
+    } catch (error) {
+        console.error("Error registering user:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+
+}
+
+const loginUser = async (req, res ) => {
+    console.log("Welcome to login route")
+}
+
+const verifyUser = async (req, res ) => {
+    console.log("Welcome to verification route")
+
+}
+
+const logoutUser = async (req, res ) => {
+    console.log("Logging out")
+}
+
+const  forgotPassword = async (req, res ) => {
+    console.log("User have forgotten his password")
+}
+
+const resetPassword = async (req, res) => {
+    console.log("Reset the password")
+}
+
+export {
+    registerUser,
+    loginUser,
+    verifyUser,
+    logoutUser,
+    forgotPassword,
+    resetPassword
+}
