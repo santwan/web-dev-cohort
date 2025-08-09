1a0019848899ea318a18165a6e354947a7404629
santwan <santwanpathak1@gmail.com>
2025-08-10 01:33:09 +0530

    Add initial route files for API endpoints
    Created route files for authentication, healthcheck, notes, projects, and tasks. Only the healthcheck route is implemented with a controller; others are initialized for future development.

---
+++ Phase 3 Node Ninja/Project Central/src/routes/healthcheck.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import {
+    healthCheck,
+} from "../controllers/healthcheck.controllers.js"
+
+const router = Router()
+
+router.get("/", healthCheck);
+
+
+export default router
