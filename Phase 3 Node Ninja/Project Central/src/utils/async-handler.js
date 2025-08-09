de1d3a701c5ac8fb38aa9cbe4ccba10592963c94
santwan <santwanpathak1@gmail.com>
2025-08-10 01:33:34 +0530

    Add commented asyncHandler utility function
    Added a commented-out asyncHandler function in utils for potential future use. This utility wraps request handlers to catch errors in async functions and pass them to Express error handling middleware.

---
+++ Phase 3 Node Ninja/Project Central/src/utils/async-handler.js
@@ -0,0 +1,10 @@
+//! we may need it later
+// const asyncHandler = (requestHandler) => {
+//     return (req, res, next) => {
+//         Promise
+//             .resolve(requestHandler(req, res, next))
+//             .catch((err) => next(err))
+//     }
+// }
+
+// export {asyncHandler}
