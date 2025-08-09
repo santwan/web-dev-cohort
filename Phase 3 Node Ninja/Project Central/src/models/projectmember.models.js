8a5c0d4754d927dcafdc09aad1865d5ce1877ed1
santwan <santwanpathak1@gmail.com>
2025-08-09 20:18:11 +0530

    Add ProjectMember Mongoose model
    Introduces a new Mongoose schema and model for project members, including references to User and Project, and a role field with enum validation.

---
+++ Phase 3 Node Ninja/Project Central/src/models/projectmember.models.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const projectMemberSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    Project: {
+        type: Schema.Type.ObjectId,
+        ref: "Project",
+        required: true
+    },
+    role: {
+        type: String,
+        enum: AvailableUserRoles,
+        default: UserRolesEnum.MEMBER,
+        
+    }
+})
+
+export const ProjectMember = mongoose.model("ProjectMember", projectMemberSchema)
